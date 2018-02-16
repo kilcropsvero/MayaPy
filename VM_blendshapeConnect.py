e66c7af7748006f3e907777c441dfebe475a5196
Vero Morera <34047156+kilcropsvero@users.noreply.github.com>
2018-02-15 21:55:43 -0600

    Update VM_blendshapeConnect.py

--- VM_blendshapeConnect.py
+++ VM_blendshapeConnect.py
@@ -13,18 +13,22 @@ def UI():
     if (mc.window(WIN, exists=True)):
         mc.deleteUI(WIN)
 
-    mc.window(WIN, title="VM_blendShape Connect", w=450, h=200, sizeable=True)
-    mc.columnLayout(rowSpacing=5)
-    mc.textFieldGrp('blend1', w=800, label='Positive blendshape. Ex: BS.smile', text='')
-    mc.textFieldGrp('blend2', label='Negative blendshape. Ex: BS.frown', text='')
-    mc.textFieldButtonGrp('control', label='Control', text='', buttonLabel='<<')
+    mc.window(WIN, title="VM_blendShape Connect", w=460, h=200, sizeable=True)
+    mc.columnLayout()
+    mc.text('1- Write the full blendshape name you want to connect. Example: BS.smile, BS.sad')
+    mc.text('2- Write the name of the control')
+    mc.text('3- Be carefull with your ortography!!')
+    mc.separator(h=20)
+    mc.textFieldGrp('blend1', label='Positive blendshape.', text='')
+    mc.textFieldGrp('blend2', label='Negative blendshape.', text='')
+    mc.textFieldGrp('control', label='Control', text='')
     mc.text('Choose the axis:')
     mc.radioCollection()
     mc.radioButton('axisX',label='X')
     mc.radioButton('axisY', label='Y')
     mc.setParent('..')
     mc.columnLayout()
-    mc.button(label='Connect the blendshapes to the GUI', w=420, h=40, bgc=(0.2, 0.2, 0.2), command=connectBS)
+    mc.button(label='Connect the blendshapes to the GUI', w=450, h=40, bgc=(0.2, 0.2, 0.2), command=connectBS)
 
     mc.showWindow(WIN)
 
@@ -33,7 +37,7 @@ def connectBS(*args):
 
     bs1= mc.textFieldGrp("blend1", query=True, text=True)
     bs2= mc.textFieldGrp("blend2", query=True, text=True)
-    cc = mc.textFieldButtonGrp('control', query=True, text=True)
+    cc = mc.textFieldGrp('control', query=True, text=True)
 
 
     if mc.textFieldGrp("blend1", query=True, text=True) and mc.textFieldGrp("blend2", query=True, text=True):
